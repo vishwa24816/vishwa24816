4d5d3e0a14acda884a7fde61f36fad3efa43cb36
Vishwa Lingam <vishwalingam24816@gmail.com>
2025-06-12 14:05:50 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <WatchlistSection

--- src/lib/mockData.ts
+++ src/lib/mockData.ts
@@ -15,6 +15,11 @@ export const mockStocks: Stock[] = [
   { id: 'icicibank', symbol: 'ICICIBANK', name: 'ICICI Bank Ltd.', price: 925.30, change: -1.05, changePercent: -0.11, exchange: 'BSE' },
   { id: 'sbin', symbol: 'SBIN', name: 'State Bank of India', price: 570.80, change: 3.40, changePercent: 0.60, exchange: 'NSE' },
   { id: 'bajfinance', symbol: 'BAJFINANCE', name: 'Bajaj Finance Ltd.', price: 7100.20, change: 50.90, changePercent: 0.72, exchange: 'NSE' },
+  { id: 'hul', symbol: 'HUL', name: 'Hindustan Unilever Ltd.', price: 2550.00, change: 10.25, changePercent: 0.40, exchange: 'NSE' },
+  { id: 'lt', symbol: 'LT', name: 'Larsen & Toubro Ltd.', price: 2200.75, change: -15.50, changePercent: -0.70, exchange: 'BSE' },
+  { id: 'axisbank', symbol: 'AXISBANK', name: 'Axis Bank Ltd.', price: 950.60, change: 5.10, changePercent: 0.54, exchange: 'NSE' },
+  { id: 'kotakbank', symbol: 'KOTAKBANK', name: 'Kotak Mahindra Bank Ltd.', price: 1800.00, change: 12.00, changePercent: 0.67, exchange: 'NSE' },
+  { id: 'itc', symbol: 'ITC', name: 'ITC Ltd.', price: 450.20, change: 2.15, changePercent: 0.48, exchange: 'BSE' },
 ];
 
 export const mockCryptoAssets: Stock[] = [
