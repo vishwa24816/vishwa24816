d6a6187155bac54ee6adc9ed112d8fb8ddd4abbd
Vishwa Lingam <vishwalingam24816@gmail.com>
2025-06-12 14:42:58 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <WatchlistSection

--- src/lib/mockData/stocks.ts
+++ src/lib/mockData/stocks.ts
@@ -14,4 +14,10 @@ export const mockStocks: Stock[] = [
   { id: 'axisbank', symbol: 'AXISBANK', name: 'Axis Bank Ltd.', price: 950.60, change: 5.10, changePercent: 0.54, exchange: 'NSE' },
   { id: 'kotakbank', symbol: 'KOTAKBANK', name: 'Kotak Mahindra Bank Ltd.', price: 1800.00, change: 12.00, changePercent: 0.67, exchange: 'NSE' },
   { id: 'itc', symbol: 'ITC', name: 'ITC Ltd.', price: 450.20, change: 2.15, changePercent: 0.48, exchange: 'BSE' },
+  { id: 'bhartiairtel', symbol: 'BHARTIARTL', name: 'Bharti Airtel Ltd.', price: 850.75, change: 7.20, changePercent: 0.85, exchange: 'NSE' },
+  { id: 'maruti', symbol: 'MARUTI', name: 'Maruti Suzuki India Ltd.', price: 9500.00, change: -45.10, changePercent: -0.47, exchange: 'NSE' },
+  { id: 'asianpaint', symbol: 'ASIANPAINT', name: 'Asian Paints Ltd.', price: 3100.50, change: 15.25, changePercent: 0.49, exchange: 'BSE' },
+  { id: 'wipro', symbol: 'WIPRO', name: 'Wipro Ltd.', price: 410.80, change: 3.10, changePercent: 0.76, exchange: 'NSE' },
+  { id: 'sunpharma', symbol: 'SUNPHARMA', name: 'Sun Pharmaceutical Industries Ltd.', price: 1150.20, change: -8.60, changePercent: -0.74, exchange: 'NSE' },
 ];
+
