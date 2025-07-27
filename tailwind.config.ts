71e341ec171d967cb935d5e8621c434d16091125
Vishwa Lingam <vishwalingam24816@gmail.com>
2025-07-27 15:44:42 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardHeader>
    
    with

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -65,9 +65,9 @@ export default {
         },
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        lg: `1rem`,
+        md: `calc(1rem - 4px)`,
+        sm: `calc(1rem - 8px)`,
       },
       keyframes: {
         'accordion-down': {
