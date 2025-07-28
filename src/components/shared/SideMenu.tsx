0e2bdff7bf8796020f534d9cac7eeca4054197e3
Vishwa Lingam <vishwalingam24816@gmail.com>
2025-07-28 06:44:46 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="g

--- src/components/shared/SideMenu.tsx
+++ src/components/shared/SideMenu.tsx
@@ -116,10 +116,10 @@ export function SideMenu() {
             <Button
               variant="ghost"
               className="justify-start text-base p-3 hover:bg-accent/10"
-              onClick={() => alert('Advanced Analytics feature coming soon!')}
+              onClick={() => alert('Analytics feature coming soon!')}
             >
               <TrendingUp className="mr-3 h-5 w-5 text-primary" />
-              Advanced Analytics
+              Analytics
             </Button>
           </SheetClose>
           <SheetClose asChild>
