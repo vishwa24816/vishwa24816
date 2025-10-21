e145fb1f2a803b596a5c01b22d2bc018ecb12ea4
Vishwa Lingam <vishwalingam24816@gmail.com>
2025-10-21 10:08:11 +0000

    Make the following changes:
    Find this element:
    
      <Button variant="ghos

--- src/components/shared/SideMenu.tsx
+++ src/components/shared/SideMenu.tsx
@@ -154,16 +154,6 @@ export function SideMenu({ onNavigate }: { onNavigate?: (view: MainView) => void
                 Family Account
             </Button>
           </SheetClose>
-          <SheetClose asChild>
-            <Button
-                variant="ghost"
-                className="justify-start text-base p-3 hover:bg-accent/10"
-                onClick={() => router.push('/nocode-algo')}
-            >
-                <Puzzle className="mr-3 h-5 w-5 text-primary" />
-                No-Code Algo
-            </Button>
-          </SheetClose>
            <SheetClose asChild>
             <Button
               variant="ghost"
