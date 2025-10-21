a72978121ead35c3ec836aa871e0400c831112b3
Vishwa Lingam <vishwalingam24816@gmail.com>
2025-10-21 12:51:39 +0000

    Make the loading sxreen muxh bigger xovering half of the width and heigh

--- src/components/shared/ProtectedRoute.tsx
+++ src/components/shared/ProtectedRoute.tsx
@@ -6,18 +6,14 @@ import { useRouter } from 'next/navigation';
 import { useAuth } from '@/contexts/AuthContext';
 import Image from 'next/image';
 
-interface ProtectedRouteProps {
-  children: ReactNode;
-}
-
 const LoadingSpinner = () => (
     <div className="flex min-h-screen items-center justify-center p-4 bg-background">
         <Image
             src="https://img.notionusercontent.com/s3/prod-files-secure%2F7526fb47-9c52-81ef-9df9-00033e87bb4d%2F44533ca6-21d9-4c13-919e-f6d332ea4fc4%2FScreenshot_20250505-214836-643_(1)_(1).png/size/w=680?exp=1761137242&sig=n1ZaahjC9NRyI-xqNn1VtzAQCL8OANTioQvhbjQbIlY&id=2916fb47-9c52-80cf-8195-fcb3959ede09&table=block"
             alt="Loading SIM"
-            width={80}
-            height={80}
-            className="animate-spin-slow"
+            width={300}
+            height={300}
+            className="animate-spin-slow w-[50vmin] h-[50vmin] max-w-[300px] max-h-[300px]"
             unoptimized
         />
     </div>
